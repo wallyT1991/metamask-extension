b12b2e50eec77fc6dcccb2048acc9259e2becd34
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-10-19 01:23:37 +0100

    Lint fixes.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/components/app/modals/transaction-error-details-modal/transaction-error-details-modal.test.js
+++ ui/components/app/modals/transaction-error-details-modal/transaction-error-details-modal.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Tooltip from '../../../ui/tooltip';
-import Button from '../../../ui/button'
-import Copy from '../../../ui/icon/copy-icon.component';
-import TransactionErrorDetailsModal from './transaction-error-details';
 import configureMockStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
-import { mountWithRouter } from '../../../../../test/lib/render-helpers';
 import { act } from '@testing-library/react';
+import Tooltip from '../../../ui/tooltip';
+import Button from '../../../ui/button';
+import Copy from '../../../ui/icon/copy-icon.component';
+import { mountWithRouter } from '../../../../../test/lib/render-helpers';
+import TransactionErrorDetailsModal from './transaction-error-details';
 
 describe('Transaction Error Details Modal', () => {
   let wrapper;
@@ -15,13 +15,13 @@ describe('Transaction Error Details Modal', () => {
     metamask: {
       provider: {
         type: 'test',
-      }
+      },
     },
-    copied: false
+    copied: false,
   };
 
   // needed for clipboard
-  window.prompt = jest.fn();
+  jest.spyOn(window, 'prompt').mockImplementation();
 
   const store = configureMockStore()(mockStore);
   const props = {
@@ -34,35 +34,33 @@ describe('Transaction Error Details Modal', () => {
       <Provider store={store}>
         <TransactionErrorDetailsModal.WrappedComponent {...props} />
       </Provider>,
-      store
+      store,
     );
   });
 
   it('shows account details modal', () => {
-    const div = wrapper.find(".transaction-error-details__main");
+    const div = wrapper.find('.transaction-error-details__main');
     expect(div.children()).toHaveLength(4);
     expect(div.childAt(0).name()).toStrictEqual('div');
     const tooltip = div.find(Tooltip);
-    expect(tooltip.first().prop('title')).toStrictEqual(
-      "[copyToClipboard]",
-    );
+    expect(tooltip.first().prop('title')).toStrictEqual('[copyToClipboard]');
     expect(tooltip.children()).toHaveLength(1);
     expect(tooltip.first().children()).toHaveLength(1);
     expect(div.find(Copy).prop('size')).toStrictEqual(17);
 
-    const h2 = div.find(".transaction-error-details__header__title").childAt(0);
-    expect(h2.prop('children')).toStrictEqual("[details]");
+    const h2 = div.find('.transaction-error-details__header__title').childAt(0);
+    expect(h2.prop('children')).toStrictEqual('[details]');
 
     const copyButton = tooltip.find(Button);
     expect(copyButton).toBeDefined();
     expect(copyButton).toHaveLength(1);
-    const mockCopyEvent = { preventDefault: jest.fn() } 
-    act(() => copyButton.prop('onClick')(mockCopyEvent));  
+    const mockCopyEvent = { preventDefault: jest.fn() };
+    act(() => copyButton.prop('onClick')(mockCopyEvent));
 
-    const closeButton = div.find(".popover-header__button");
+    const closeButton = div.find('.popover-header__button');
     expect(closeButton).toBeDefined();
     expect(closeButton).toHaveLength(1);
-    const mockCloseEvent = { preventDefault: jest.fn() } 
-    act(() => closeButton.prop('onClick')(mockCloseEvent));      
+    const mockCloseEvent = { preventDefault: jest.fn() };
+    act(() => closeButton.prop('onClick')(mockCloseEvent));
   });
 });
--- ui/components/app/modals/transaction-error-details-modal/transaction-error-details.js
+++ ui/components/app/modals/transaction-error-details-modal/transaction-error-details.js
@@ -1,54 +1,60 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import { useI18nContext } from '../../../../hooks/useI18nContext';
-import Tooltip from '../../../ui/tooltip'
-import Button from '../../../ui/button'
+import Tooltip from '../../../ui/tooltip';
+import Button from '../../../ui/button';
 import Copy from '../../../ui/icon/copy-icon.component';
 import { hideModal } from '../../../../store/actions';
-import { useDispatch } from 'react-redux';
 import withModalProps from '../../../../helpers/higher-order-components/with-modal-props';
 import { useCopyToClipboard } from '../../../../hooks/useCopyToClipboard';
 
-const TransactionErrorDetailsModal = ({message}) => {
+const TransactionErrorDetailsModal = ({ message }) => {
   const [copied, handleCopy] = useCopyToClipboard();
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const t = useI18nContext();
+  const t = useI18nContext();
 
-    const handleClose = () => {
-      dispatch(hideModal());
-    }
+  const handleClose = () => {
+    dispatch(hideModal());
+  };
 
-    return (
-        <div className="transaction-error-details__main">
-          <div className="transaction-error-details__header">
-            <div className="transaction-error-details__header__title">
-              <h2>{t('details')}</h2>
-              <button
-                  className="fas fa-times popover-header__button"
-                  title={t('close')}
-                  data-testid="popover-close"
-                  onClick={() => handleClose()}
-              />           
-            </div>            
-          </div>
-          <hr />
-          <p>{message}</p>
-          <br />
-          <Tooltip
-            position="bottom"
-            title={
-              copied ? t('copiedExclamation') : t('copyToClipboard')
-            }
-          >
-            <Button type="link" onClick={() => {
-                handleCopy(message);
-            }}>
-              <Copy size={17} color="#3098DC" />
-              {t('copyToClipboard')}
-            </Button>
-          </Tooltip>
+  return (
+    <div className="transaction-error-details__main">
+      <div className="transaction-error-details__header">
+        <div className="transaction-error-details__header__title">
+          <h2>{t('details')}</h2>
+          <button
+            className="fas fa-times popover-header__button"
+            title={t('close')}
+            data-testid="popover-close"
+            onClick={() => handleClose()}
+          />
         </div>
-      );
-}
+      </div>
+      <hr />
+      <p>{message}</p>
+      <br />
+      <Tooltip
+        position="bottom"
+        title={copied ? t('copiedExclamation') : t('copyToClipboard')}
+      >
+        <Button
+          type="link"
+          onClick={() => {
+            handleCopy(message);
+          }}
+        >
+          <Copy size={17} color="#3098DC" />
+          {t('copyToClipboard')}
+        </Button>
+      </Tooltip>
+    </div>
+  );
+};
 
-export default withModalProps(TransactionErrorDetailsModal)
+TransactionErrorDetailsModal.propTypes = {
+  message: PropTypes.string,
+};
+
+export default withModalProps(TransactionErrorDetailsModal);
