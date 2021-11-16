442ce45f8b92c62e32f6bd2a8b182978b9ef62f1
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-11-16 16:54:30 +0400

    Nit: This branch is unnecessary; appState is never falsy. The transactionsToDisplayOnFailure property is always initialized as well, so we don't need that default either.
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/store/actions.js
+++ ui/store/actions.js
@@ -1051,7 +1051,7 @@ export function updateMetamaskState(newState) {
     const { currentNetworkTxList } = getState().metamask;
     const { currentNetworkTxList: newNetworkTxList } = newState;
 
-    const { transactionsToDisplayOnFailure = {} } = appState || {};
+    const { transactionsToDisplayOnFailure } = appState;
 
     if (currentLocale && newLocale && currentLocale !== newLocale) {
       dispatch(updateCurrentLocale(newLocale));
